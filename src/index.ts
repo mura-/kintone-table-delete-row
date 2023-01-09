e7896fe47373b5b26a1ed3fb8dd890b715172e19
Kazuki Murahama <kazuki.murahama@gmail.com>
2023-01-10 00:40:43 +0900

    複数のテーブルの行を消せるようにした

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,28 @@
 ((PLUGIN_ID) => {
   kintone.events.on(['app.record.create.show', 'app.record.edit.show'], (event) => {
 
-    const tableBody = document.querySelector<HTMLTableElement>('table.subtable-gaia > tbody');
-    if(!tableBody) return event;
+    // すべてのテーブルを取得し監視する
+    const tableBodies = document.querySelectorAll<HTMLTableElement>('table.subtable-gaia > tbody') 
+    tableBodies.forEach((tableBody) => {
 
-    let observer = new MutationObserver(mutationRecords => {
-      console.log(mutationRecords, tableBody.rows.length); // console.log(the changes)
-      switch (tableBody.rows.length) {
-        // TODO: 0の場合にthにボタンを表示する処理
-        case 1: {
-          const removeRowButton = tableBody.querySelector<HTMLButtonElement>('tr > td.subtable-operation-gaia > button.remove-row-image-gaia');
-          if(removeRowButton) removeRowButton.style.display = 'inline-block';
+      let observer = new MutationObserver(mutationRecords => {
+        switch (tableBody.rows.length) {
+          // TODO: 0の場合にthにボタンを表示する処理
+          case 1: {
+            // 削除ボタンを取得し表示させる
+            const removeRowButton = tableBody.querySelector<HTMLButtonElement>('tr > td.subtable-operation-gaia > button.remove-row-image-gaia');
+            if(removeRowButton) removeRowButton.style.display = 'inline-block';
+          }
+          default:
+            return;
         }
-        default:
-          return;
-      }
-    });
+      });
     
-    observer.observe(tableBody, {
-      childList: true,
+      observer.observe(tableBody, {
+        childList: true,
+      });
     });
+
     return event;
   });
 })(kintone.$PLUGIN_ID);

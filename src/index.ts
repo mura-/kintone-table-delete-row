0edcbfd43f71dd3c9acc5cfb3575cf517208fe14
Kazuki Murahama <kazuki.murahama@gmail.com>
2023-01-10 00:36:09 +0900

    最初のテーブルの行のみ消せるようにした

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
 ((PLUGIN_ID) => {
-  kintone.events.on('app.record.index.show', (event) => {
-    console.log('これはプラグインのテスト')
+  kintone.events.on(['app.record.create.show', 'app.record.edit.show'], (event) => {
+
+    const tableBody = document.querySelector<HTMLTableElement>('table.subtable-gaia > tbody');
+    if(!tableBody) return event;
+
+    let observer = new MutationObserver(mutationRecords => {
+      console.log(mutationRecords, tableBody.rows.length); // console.log(the changes)
+      switch (tableBody.rows.length) {
+        // TODO: 0の場合にthにボタンを表示する処理
+        case 1: {
+          const removeRowButton = tableBody.querySelector<HTMLButtonElement>('tr > td.subtable-operation-gaia > button.remove-row-image-gaia');
+          if(removeRowButton) removeRowButton.style.display = 'inline-block';
+        }
+        default:
+          return;
+      }
+    });
+    
+    observer.observe(tableBody, {
+      childList: true,
+    });
     return event;
   });
 })(kintone.$PLUGIN_ID);
